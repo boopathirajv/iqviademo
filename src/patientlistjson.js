adc9cb8e550c8a7d0ea544b1965a7e2ec35630f3
boopathiraj.v <boopathiraj@skava.com>
2019-11-20 14:29:53 +0530

    ui fix

--- src/patientlistjson.js
+++ src/patientlistjson.js
@@ -5,7 +5,7 @@ const list =
     "mname":"josh",
     "lname": "sekar",
     "surname": "pachan",
-    
+    "status":"alive",
     "contact": "9865432134",
     "dob": "13/05/1985 34 years"   
     
